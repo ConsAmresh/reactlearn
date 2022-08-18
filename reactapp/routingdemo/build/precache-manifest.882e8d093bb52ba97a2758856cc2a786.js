e53237a11968ff449a79df4d13e9b8fcd2dae563
amresh <amresh.rolla@gmail.com>
2022-08-19 00:49:38 +0530

    test

---
+++ reactapp/routingdemo/build/precache-manifest.882e8d093bb52ba97a2758856cc2a786.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf7ca41d946e44a88ad7",
+    "url": "/static/css/main.002d2ec3.chunk.css"
+  },
+  {
+    "revision": "bf7ca41d946e44a88ad7",
+    "url": "/static/js/main.bf7ca41d.chunk.js"
+  },
+  {
+    "revision": "00d95f85c58c173709ce",
+    "url": "/static/css/1.2bf4d30e.chunk.css"
+  },
+  {
+    "revision": "00d95f85c58c173709ce",
+    "url": "/static/js/1.00d95f85.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9fd1f8d88504f71c98428ff2302e5ff6",
+    "url": "/index.html"
+  }
+];
